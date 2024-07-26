52d05dfc47b4688187831b89f6c560a18ae628b3
Shrey Sheladia <shrey.sheladia@gmail.com>
2024-07-26 15:39:27 -0700

    agent to format context and question

--- language_model.py
+++ language_model.py
@@ -19,14 +19,19 @@ class LanguageModel(ABC):
 
 
 class OpenAILanguageModel(LanguageModel):
-    def __init__(self, api_key: str):
+    def __init__(self, api_key: str, sys_prompt: str | bool = None):
         self.client = OpenAI(api_key=api_key)
-        self._messages = [
-            {"role": "system", "content": SYS_PROMPT},
-        ]
+        if sys_prompt is not False:
+            sys_prompt = SYS_PROMPT if not sys_prompt else sys_prompt
+            self._messages = [
+                {"role": "system", "content": SYS_PROMPT},
+            ]
+        else:
+            self._messages = []
 
     def chat(self, prompt: str, stream: bool = False):
         self._messages.append({"role": "user", "content": prompt})
+        write_messages_to_file(self._messages)
         response = self.client.chat.completions.create(
             model="gpt-4o-mini",
             messages=self._messages,
@@ -34,7 +39,7 @@ class OpenAILanguageModel(LanguageModel):
         )
         if not stream:
             response = response.choices[0].message.content
-            self._messages.append({"role": "system", "content": response})
+            self._messages.append({"role": "assistant", "content": response})
 
             return response
         else:
@@ -44,8 +49,16 @@ class OpenAILanguageModel(LanguageModel):
                 full_message += response_chunk if response_chunk else ""
                 yield response_chunk
 
-            self._messages.append({"role": "system", "content": full_message})
+            self._messages.append({"role": "assistant", "content": full_message})
+            write_messages_to_file(self._messages)
             yield False
+
+    def set_message_history(self, messages: list[dict]):
+        self._messages = messages
+        write_messages_to_file(self._messages)
+
+    def get_message_history(self) -> list[dict]:
+        return self._messages
 
     def reset(self, sys_prompt: str = None) -> None:
         if not sys_prompt:
@@ -53,3 +66,9 @@ class OpenAILanguageModel(LanguageModel):
         self._messages = [
             {"role": "system", "content": sys_prompt},
         ]
+
+
+def write_messages_to_file(messages: list[dict]):
+    with open("message_history.txt", "w") as file:
+        for message in messages:
+            file.write(f"{message['role']}: {message['content']}\n\n")
--- rag_llm.py
+++ rag_llm.py
@@ -6,7 +6,11 @@ from dotenv import load_dotenv
 
 from document_loader import DocumentStore
 from language_model import LanguageModel, OpenAILanguageModel
-from prompt_format import PROMPT_FORMAT
+from prompt_format import NEW_AGENT_PROMPT, PROMPT_FORMAT
+
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 class RAGLanguageModel:
@@ -19,7 +23,8 @@ class RAGLanguageModel:
     ):
         self._api_key = api_key
         self._path = document_folder
-        self._llm = llm(api_key=self._api_key)
+        self._llm = llm
+        self._agent = self._llm(api_key=self._api_key)
         self._document_store = document_store(path=self._path, api_key=self._api_key)
         self._document_store.load_documents_to_index()
 
@@ -29,25 +34,77 @@ class RAGLanguageModel:
             if question.lower() == "q":
                 break
 
-            context = self._document_store.search(question)
+            if len(self._agent.get_message_history()) > 1:
+                new_question = self._add_context_to_question(question)
+            else:
+                new_question = question
+            context = self._document_store.search(new_question)
             formatted_context = self._format_context(context)
-            prompt = PROMPT_FORMAT.format(formatted_context, question)
-            response = self._llm.chat(prompt=prompt, stream=True)
+            prompt = PROMPT_FORMAT.format(formatted_context, new_question)
+
+            full_response = ""
+            response = self._agent.chat(prompt=prompt, stream=True)
             for chunk in response:
                 if chunk:
+                    full_response += chunk
                     print(chunk, end="")
                 elif chunk is False:
                     print("\n")
                     break
+            self._change_message_history(new_question, full_response)
 
     def _format_context(self, context: list) -> str:
-        formatted_contect = ""
+        formatted_context = ""
         for index, document in enumerate(context):
-            formatted_contect += f"Document {index + 1})\n"
-            formatted_contect += document.page_content
-            formatted_contect += "\n\n"
+            formatted_context += f"Document {index + 1})\n"
+            formatted_context += document.page_content
+            formatted_context += "\n\n"
 
-        return formatted_contect
+        return formatted_context
+
+    def _add_context_to_question(self, question: str) -> str:
+        new_agent = self._create_agent()
+        message_history = self._agent.get_message_history()
+        formatted_message_history = self._format_message_history(message_history)
+        agent_prompt = NEW_AGENT_PROMPT.replace("_USER_QUESTION_", question)
+        agent_prompt = agent_prompt.replace(
+            "_MESSAGE_HISTORY_", formatted_message_history
+        )
+        response = new_agent.chat(prompt=agent_prompt, stream=True)
+        new_question = ""
+        for chunk in response:
+            if chunk:
+                new_question += chunk
+            elif chunk is False:
+                break
+
+        return new_question
+
+    def _format_message_history(self, message_history: list[dict]) -> str:
+        formatted_message_history = ""
+        for message in message_history:
+            role = message["role"]
+            content = message["content"]
+            formatted_message_history += f"{role}: {content}\n"
+
+        return formatted_message_history
+
+    def _change_message_history(self, user_question: str, ai_response: str) -> str:
+        current_messages = self._agent.get_message_history()
+        latest_user_message = current_messages[-2]
+        latest_ai_message = current_messages[-1]
+
+        latest_user_message["content"] = user_question
+        latest_ai_message["content"] = ai_response
+
+        current_messages[-2] = latest_user_message
+        current_messages[-1] = latest_ai_message
+
+        self._agent.set_message_history(current_messages)
+
+    def _create_agent(self) -> OpenAILanguageModel:
+        new_agent = self._llm(api_key=self._api_key, sys_prompt=False)
+        return new_agent
 
 
 if __name__ == "__main__":
